83f2c6bed7480c55d0bf31058ae1f72ef60a41bc
uma-116 <yumasato116@gmail.com>
2020-06-02 15:01:57 +0900

    ユーザー登録機能関連

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,12 @@
 class PostsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+
 
   def index
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
   
 end
