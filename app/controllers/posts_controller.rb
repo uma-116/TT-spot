81eb0c070de69b9cdb4d118179ca00709d3c9892
uma-116 <yumasato116@gmail.com>
2020-06-02 23:33:43 +0900

    投稿機能WIP

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,8 +6,28 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def new
+    @post = Post.new
+    # @post.images.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      # @item.images.new
+      render :new
+    end
+  end
+
   def move_to_index
     redirect_to action: :index unless user_signed_in?
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:name, :content, :image)
   end
   
 end
