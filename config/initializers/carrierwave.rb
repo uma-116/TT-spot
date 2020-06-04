fe690efcb692385fedc6a710d8fa8322ea5b98dc
uma-116 <yumasato116@gmail.com>
2020-06-04 12:22:23 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,16 +2,18 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
-CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.credentials.aws_access_key_id,
-    aws_secret_access_key: Rails.application.credentials.aws_secret_access_key,
-    region: 'ap-northeast-1'
-  }
-
-  config.fog_directory  = 'ttspot'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ttspot'
-end
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    if Rails.env.production?
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider:              'AWS',
+      aws_access_key_id:     Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+      region:                'ap-northeast-1'
+    }
+    config.fog_directory  = 'ttspot'
+    config.fog_public     = true
+    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
+    end
+  end
