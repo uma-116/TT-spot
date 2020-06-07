44b2a8012d80be058546af9111cf6ef3ac1b5dc5
uma-116 <yumasato116@gmail.com>
2020-06-07 20:26:14 +0900

    マイページController, routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   root 'posts#index'
 
   resources :posts, only: [:index, :new, :create, :show, :update, :edit, :destroy]
+
+  resources :users, only: [:show]
+  
 end
