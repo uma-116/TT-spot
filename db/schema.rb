23244d4fedbd2898ac9b3690afb1621a0c292b14
uma-116 <yumasato116@gmail.com>
2020-06-13 20:30:52 +0900

    add column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_05_070836) do
+ActiveRecord::Schema.define(version: 2020_06_13_112033) do
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "img", null: false
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2020_06_05_070836) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "station"
+    t.string "url"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
